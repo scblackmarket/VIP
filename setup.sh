5a5bb622f820db301af0b186cdf07713e5632bd0
scblackmarket <korek6175@gmail.com>
2025-01-01 14:00:53 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -738,7 +738,7 @@ TEXT="
 <code>EXP SCRIPT : </code><code>$EXP Days</code>
 <code>━━━━━━━━━━━━━━━━━━━━</code>
 <i> Notifikasi Installer Script...</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"🔥ᴏʀᴅᴇʀ","url":"https://t.me/memek"},{"text":"🔥GRUP","url":"https://t.me/memek"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"🔥ᴏʀᴅᴇʀ","url":"https://t.me/ADJstoreVpn"},{"text":"🔥GRUP","url":"https://t.me/configAdijaya"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 clear
 }
